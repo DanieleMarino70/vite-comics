9f89e39499b05f31caaf2444932f9f8fe1e425be
DanieleMarino70 <danielaotitan@gmail.com>
2023-02-27 16:36:09 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default{};
+</script>
+
+<template>
+ 
+    
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+    <img src="../assets/img/dc-logo.png" alt="">
+
+  </header>
+</template>
+
+<style scoped>
+
+</style>
